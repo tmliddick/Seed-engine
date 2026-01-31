b3d21eef1500c0be2f0b5c9cb09ecc9299a82f49
Tim Liddick <tmliddick@gmail.com>
2026-01-31 15:53:56 -0600

    Implement world generation function

---
+++ Src/generateWorld.ts
@@ -0,0 +1,16 @@
+import { WorldSeed, DerivedWorld } from "./types";
+import { deriveGeography } from "./logic/geography";
+import { deriveCulture } from "./logic/culture";
+import { deriveMagic } from "./logic/magic";
+import { derivePolitics } from "./logic/politics";
+import { deriveMythology } from "./logic/mythology";
+
+export function generateWorld(seed: WorldSeed): DerivedWorld {
+  return {
+    geography: deriveGeography(seed),
+    culture: deriveCulture(seed),
+    magicSystem: deriveMagic(seed),
+    politics: derivePolitics(seed),
+    mythology: deriveMythology(seed)
+  };
+}
