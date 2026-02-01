a7d461b4f1e97d13784d93f30076a0b9c9abc8d1
Tim Liddick <tmliddick@gmail.com>
2026-01-31 18:49:37 -0600

    Import and register core plugins in Index.ts

--- Src/Index.ts
+++ Src/Index.ts
@@ -1,2 +1,8 @@
+import { registerCorePlugins } from "./plugins/register"
+import { generateWorld } from "./generateWorld"
+
+registerCorePlugins()
+
+export { generateWorld }
 export * from "./types";
 export * from "./generateWorld";
